e68edef5efb3f972eeeb396649bbedb350eecdc5
isaac296 <70650670+isaac296@users.noreply.github.com>
2023-03-30 14:10:18 -0400

    Diseño general actualizado

--- apps/static/js/demo/datatables-demo.js
+++ apps/static/js/demo/datatables-demo.js
@@ -1,4 +1,18 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({language: {
+    search: "Buscar registro",
+    lengthMenu:"Mostrar _MENU_ registros",
+    paginate: {
+        "first":      "Primero",
+        "last":       "Último",
+        "next":       "Siguiente",
+        "previous":   "Anterior"
+    },
+    emptyTable:     "No hay información en esta tabla",
+    info:           "Mostrando de _START_ hasta _END_ de _TOTAL_ registros",
+    infoEmpty:      "No hay registros",
+    infoFiltered:   "(Filtrando de _MAX_ registros en total)",
+    zeroRecords:    "No hay resultados en la búsqueda",
+}});
 });
