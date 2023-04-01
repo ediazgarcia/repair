be3f72891287382bcc172736e51cc44e69a8f5b5
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2023-04-01 04:48:45 -0400

    servicios de autenticacion y autorizacion en un 90%

--- apps/__init__.py
+++ apps/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config, DevelopmentConfig, ProductionConfig
 
+
 app = Flask(__name__)
+
 
 # Cargar las configuraciones
 app.config.from_object(DevelopmentConfig)
@@ -11,7 +13,6 @@ db = SQLAlchemy(app)
 # Importar routes
 
 from apps.routes.auth import auth
-from apps.routes.dash import dash
 from apps.routes.user import user
 from apps.routes.company import company
 from apps.routes.client import client
@@ -20,7 +21,6 @@ from apps.routes.employee import employee
 from apps.routes.vehicle import vehicle
 
 app.register_blueprint(auth)
-app.register_blueprint(dash)
 app.register_blueprint(user)
 app.register_blueprint(company)
 app.register_blueprint(client)
@@ -28,7 +28,7 @@ app.register_blueprint(provider)
 app.register_blueprint(employee)
 app.register_blueprint(vehicle)
 
-app.add_url_rule("/", endpoint="index")
+# app.add_url_rule("/", endpoint="index")
 
 # Ejecutar todas las consultas SQL
 with app.app_context():
--- apps/routes/auth.py
+++ apps/routes/auth.py
@@ -2,9 +2,60 @@ from flask import (
     render_template, Blueprint, flash, g, redirect, request, session, url_for
 )
 
-# from apps import db
+from apps import app
+from werkzeug.security import check_password_hash
+from functools import wraps
 
-auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+from apps.models.user import User
+
+auth = Blueprint('auth', __name__)
 
 # Autentication (login)
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            if user.active:  # Verificar si el usuario está activo
+                session['user_id'] = user.id
+                session['username'] = user.username
+                session['role'] = user.role
+                return redirect(url_for('auth.home'))
+            else:
+                return render_template('auth/login.html', error='Your account is inactive. Please contact the administrator to activate your account.')
+        else:
+            return render_template('auth/login.html', error='Invalid username or password')
+    else:
+        return render_template('auth/login.html')
 
+    
+    
+    
+def set_role(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' in session:
+            user = User.query.get(session['user_id'])
+            g.role = session['role']
+            return f(user=user, *args, **kwargs)
+        return redirect(url_for('auth.login'))
+    return decorated_function
+
+    
+@auth.route('/', methods=['GET', 'POST'])
+@set_role
+def home(user):
+    if g.role == 'Administrador':
+        return render_template('admin/index.html', username=session['username'], user=user, role=g.role)
+    elif g.role == 'Usuario':
+        return render_template('views/index.html', username=session['username'], user=user, role=g.role)
+    return redirect(url_for('auth.login'))
+
+    
+@auth.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.login'))
