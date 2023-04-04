19d069f4fad392e66febc9c9b5b31db4a77d7e5d
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2023-04-04 01:22:44 -0400

    refactorizacion auth-login-user

--- apps/routes/employee.py
+++ apps/routes/employee.py
@@ -4,10 +4,26 @@ from flask import (
 
 from werkzeug.security import generate_password_hash
 
-#from apps.models.employee import Employee
+# from apps.models.employee import Employee
+from apps.models.user import User
 from apps import db
 
 employee = Blueprint('employee', __name__, url_prefix='/employee')
+
+# función para verificar el rol del usuario
+@employee.before_request
+def set_role():
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+        g.role = session['role']
+        g.username = session['username']
+        g.fullname = session['fullname']
+        g.email = session['email']
+    else:
+        g.role = None
+        g.username = None
+        g.fullname = None
+        g.email = None
 
 @employee.route("/list")
 def get_employee():
--- apps/routes/inventory.py
+++ apps/routes/inventory.py
@@ -5,9 +5,25 @@ from flask import (
 from werkzeug.security import generate_password_hash
 
 #from apps.models.inventory import inventory
+from apps.models.user import User
 from apps import db
 
 inventory = Blueprint('inventory', __name__, url_prefix='/inventory')
+
+# función para verificar el rol del usuario
+@inventory.before_request
+def set_role():
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+        g.role = session['role']
+        g.username = session['username']
+        g.fullname = session['fullname']
+        g.email = session['email']
+    else:
+        g.role = None
+        g.username = None
+        g.fullname = None
+        g.email = None
 
 @inventory.route("/list")
 def get_inventory():
