46069690a7dd8ed4fd9f6cb0172c8b5f02c56200
isaac296 <70650670+isaac296@users.noreply.github.com>
2023-04-10 20:52:57 -0400

    Previo Recepcion Vehiculos

--- apps/routes/vehiclereception.py
+++ apps/routes/vehiclereception.py
@@ -5,8 +5,12 @@ from flask import (
 
 from werkzeug.security import generate_password_hash
 
-# from apps.models.vehiclereception import VehicleReception
-from apps.models.user import User
+
+from sqlalchemy import func 
+from apps.models.vehicle_reception import VehicleReception
+from apps.models.vehicle_reception_details import VehicleReceptionDetail
+from apps.models.vehicle import Vehicle
+from apps.models.employee import Employee
 from apps import db
 
 vehiclereception = Blueprint(
@@ -17,10 +21,43 @@ vehiclereception = Blueprint(
 # función para verificar el rol del usuario
 @set_role
 def get_vehiclereception(user=None):
-    return render_template('admin/workshop/vehiclereception/list.html')
+    vehiclereception=VehicleReception.query.all()
+    vehiclereception_detail=VehicleReceptionDetail.query.all()
+    return render_template('admin/workshop/vehiclereception/list.html', vehiclereception=vehiclereception, vehiclereception_detail=vehiclereception_detail)
 
 
-@vehiclereception.route("/create")
+@vehiclereception.route("/create", methods=['GET', 'POST'])
 @set_role
 def create_vehiclereception(user=None):
-    return render_template('admin/workshop/vehiclereception/create.html')
+    if request.method == 'POST':
+            employee_id = request.form['employee_id']
+            vehicle_id = request.form['vehicle_id']
+            reception_reason = request.form['reception_reason']
+            #Campos Detalle
+            problem_description = request.form['problem_description']
+            front_condition = request.form['front_condition']
+            back_condition = request.form['back_condition']
+            left_condition = request.form['left_condition']
+            right_condition = request.form['right_condition']
+            roof_condition = request.form['roof_condition']
+            accessories = request.form['accessories']
+            tools =  request.form['tools']
+            objects =  request.form['objects']
+
+            new_vehiclereception = VehicleReception (employee_id = employee_id, vehicle_id = vehicle_id, reception_reason = reception_reason)
+
+            db.session.add(new_vehiclereception)
+            db.session.commit()
+            #Obtener id de la ultima recepcion
+            max_id = db.session.query(func.max(VehicleReception.id))
+            vehicle_reception_id = max_id
+            
+            #Insertar Detalle
+            new_vehiclereception_detail = VehicleReceptionDetail (problem_description = problem_description, front_condition = front_condition, back_condition = back_condition, left_condition = left_condition, right_condition = right_condition, roof_condition = roof_condition, accessories = accessories, tools = tools, objects = objects, vehicle_reception_id = vehicle_reception_id)
+            db.session.add(new_vehiclereception_detail)
+            db.session.commit()
+            
+            flash('¡Recepción de Vehículo añadida con éxito!')
+    vehicle=Vehicle.query.all()
+    employee=Employee.query.all()
+    return render_template('admin/workshop/vehiclereception/create.html', vehicle=vehicle, employee=employee)
