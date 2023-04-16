fc8cd5c94c027184c546bdc012593226f54c9e70
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2023-04-16 11:34:29 -0400

    added products in details billigs

--- apps/models/billing.py
+++ apps/models/billing.py
@@ -14,10 +14,8 @@ class Billing(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     order_num = db.Column(db.String(20), unique=True)
     type = db.Column(db.String(30), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    received = db.Column(db.Numeric(10, 2), nullable=False)
-    change = db.Column(db.Numeric(10, 2), nullable=False)
     total = db.Column(db.Numeric(10, 2), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow,)
 
     company_id = db.Column(db.Integer, db.ForeignKey(
         'companies.id', onupdate='RESTRICT', ondelete='CASCADE'))
@@ -31,19 +29,14 @@ class Billing(db.Model):
         'services_orders.id', onupdate='RESTRICT', ondelete='CASCADE'))
     orders_services = db.relationship(
         'ServiceOrder', backref=db.backref('billings', lazy=True))
-    payments_id = db.Column(db.Integer, db.ForeignKey(
-        'payments.id', onupdate='RESTRICT', ondelete='CASCADE'))
-    payments = db.relationship(
-        'Payments', backref=db.backref('billings', lazy=True))
 
-    def __init__(self, order_num, type, date, total, company, client, employee, orders_services):
+    def __init__(self, order_num, type, total, company, client, orders_services):
         self.order_num = order_num
         self.type = type
-        self.date = date
         self.total = total
+        self.created = datetime.utcnow()
         self.company = company
         self.client = client
-        self.employee = employee
         self.orders_services = orders_services
 
     def __repr__(self):
@@ -54,7 +47,6 @@ class BillingDetail(db.Model):
     __tablename__ = 'billings_details'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    description = db.Column(db.String(100), nullable=False)
     unit_price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     total = db.Column(db.Numeric(10, 2), nullable=False)
@@ -65,11 +57,10 @@ class BillingDetail(db.Model):
 
     billing_id = db.Column(db.Integer, db.ForeignKey(
         'billings.id'), nullable=False)
-    billing=db.relationship(
+    billing = db.relationship(
         'Billing', backref=db.backref('billings_details', lazy=True))
 
-    def __init__(self, description, unit_price, quantity, total, product, billing):
-        self.description = description
+    def __init__(self, unit_price, quantity, total, product, billing):
         self.unit_price = unit_price
         self.quantity = quantity
         self.total = total
@@ -77,4 +68,4 @@ class BillingDetail(db.Model):
         self.billing = billing
 
     def __repr__(self):
-        return f'Payment: {self.description} {self.unit_price} {self.quantity}'
+        return f'Payment: {self.unit_price} {self.quantity}'
--- apps/routes/billing.py
+++ apps/routes/billing.py
@@ -1,6 +1,7 @@
 from flask import (render_template, Blueprint, flash, g,
                    redirect, request, session, url_for,)
 
+import json
 # Importar el contador
 from itertools import count
 from werkzeug.security import generate_password_hash
@@ -12,7 +13,7 @@ from apps.models.user import User
 from apps.models.client import Customer
 from apps.models.company import Company
 from apps.models.employee import Employee
-from apps.models.payments import Payments, PaymentsDetails
+# from apps.models.payments import Payments
 from apps.models.products import Product
 from apps.models.orders_services import ServiceOrder
 from apps import db
@@ -31,14 +32,12 @@ def get_billing(user=None):
     employees = Employee.query.all()
     company = Company.query.all()
     orders_services = ServiceOrder.query.all()
-    payments = Payments.query.all()
-    payments_details = PaymentsDetails.query.all()
     product = Product.query.all()
 
     if g.role == 'Administrador':
-        return render_template('admin/workshop/billing/list.html', billings=billings, billing_detail=billing_detail, customers=customers, employees=employees, company=company, orders_services=orders_services, payments=payments, payments_details=payments_details, product=product)
+        return render_template('admin/workshop/billing/list.html', billings=billings, billing_detail=billing_detail, customers=customers, employees=employees, company=company, orders_services=orders_services, product=product)
     else:
-        return render_template('views/workshop/billing/list.html', billings=billings, billing_detail=billing_detail, customers=customers, employees=employees, company=company, orders_services=orders_services, payments=payments, payments_details=payments_details, product=product)
+        return render_template('views/workshop/billing/list.html', billings=billings, billing_detail=billing_detail, customers=customers, employees=employees, company=company, orders_services=orders_services, product=product)
 
 
 # Crear un contador que inicie en 100
@@ -50,17 +49,18 @@ order_num_counter = count(start=100)
 def create_billing(user=None):
     if request.method == 'POST':
         # Obtener los datos del formulario
-        type = request.form['type']
-        itbis = request.form['itbis']
-        discount = request.form['discount']
-        sub_total = request.form['sub_total']
+        type = str(request.form['type'])
         total = request.form['total']
-        status = request.form['status']
         company_id = request.form['company_id']
         client_id = request.form['client_id']
-        employee_id = request.form['employee_id']
         orders_services_id = request.form['orders_services_id']
-        payments_id = request.form['payments_id']
+
+        # Campos Detalle Facturas
+        # Crear una nueva instancia de la clase BillingDetail para cada producto en la factura
+        products_id = request.form.getlist('product_id[]')
+        quantity = request.form.getlist('quantity[]')
+        unit_price = request.form.getlist('unit_price[]')
+        total = request.form.getlist('total[]')
 
         # Generar el order_num con prefijo "RV-"
         order_num = "FT-" + str(next(order_num_counter))
@@ -68,38 +68,28 @@ def create_billing(user=None):
         # Fetch los objetos relacionados desde la base de datos
         company = Company.query.filter_by(id=company_id).first()
         client = Customer.query.filter_by(id=client_id).first()
-        employee = Employee.query.filter_by(id=employee_id).first()
+        # employee = Employee.query.filter_by(id=employee_id).first()
         orders_service = ServiceOrder.query.filter_by(
             id=orders_services_id).first()
-        payments = ServiceOrder.query.filter_by(id=payments_id).first()
+        # payments = ServiceOrder.query.filter_by(id=payments_id).first()
 
         # Crear una instancia de la clase Billing
-        billing = Billing(order_num=order_num, type=type, itbis=itbis, discount=discount, sub_total=sub_total, total=total, status=status,
-                          company=company, client=client, employee=employee,
-                          orders_service=orders_service, payments=payments)
+        billing = Billing(order_num=order_num, type=type, total=total,
+                          company=company, client=client,
+                          orders_service=orders_service)
 
         # Guardar la factura en la base de datos
         db.session.add(billing)
         db.session.commit()
 
-        # Campos Detalle Facturas
-        # Crear una nueva instancia de la clase BillingDetail para cada producto en la factura
-        # Suponiendo que los productos se pasan como una lista en el formulario con claves 'product_id[]', 'lot[]', 'itbis[]', 'discount[]', 'sub_total[]' y 'total[]'
-        product_ids = request.form.getlist('product_id[]')
-        lots = request.form.getlist('lot[]')
-        itbis = request.form.getlist('itbis[]')
-        discounts = request.form.getlist('discount[]')
-        sub_totals = request.form.getlist('sub_total[]')
-        totals = request.form.getlist('total[]')
-
         # Obtener id de la ultima recepcion
         # max_id = db.session.query(func.max(BillingDetail.id)).scalar()
 
         # Crear instancias de la clase BillingDetail y asociarlas con la factura creada anteriormente
-        for i in range(len(product_ids)):
-            product = Product.query.filter_by(id=product_ids[i]).first()
-            billing_detail = BillingDetail(lot=lots[i], itbis=itbis[i], discount=discounts[i],
-                                           sub_total=sub_totals[i], total=totals[i], product=product, billing=billing)
+        for i in range(len(products_id)):
+            product = Product.query.filter_by(id=products_id[i]).first()
+            billing_detail = BillingDetail(unit_price=unit_price[i], quantity=quantity[i],
+                                           total=total[i], product=product, billing=billing)
             db.session.add(billing_detail)
 
         db.session.commit()
@@ -108,17 +98,14 @@ def create_billing(user=None):
     billing_detail = BillingDetail.query.all()
     customers = Customer.query.all()
     employees = Employee.query.all()
-    company = Company.query.all()
+    companies = Company.query.all()
     orders_services = ServiceOrder.query.all()
-    payments = Payments.query.all()
-    payments_details = PaymentsDetails.query.all()
+    # payments = Payments.query.all()
     product = Product.query.all()
 
     if g.role == 'Administrador':
         return render_template('admin/workshop/billing/create.html', billings=billings, billing_detail=billing_detail,
-                               customers=customers, employees=employees, company=company, orders_services=orders_services,
-                               payments=payments, payments_details=payments_details, product=product)
+                               customers=customers, companies=companies, orders_services=orders_services, product=product)
     else:
         return render_template('views/workshop/billing/create.html', billings=billings, billing_detail=billing_detail,
-                               customers=customers, employees=employees, company=company, orders_services=orders_services,
-                               payments=payments, payments_details=payments_details,  product=product)
+                               customers=customers, companies=companies, orders_services=orders_services, product=product)
