67217321c88fa9a96efbed2226dea075f56337ed
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2023-04-16 11:34:29 -0400

    added products in details billigs

--- apps/models/payments.py
+++ apps/models/payments.py
@@ -16,27 +16,3 @@ class Payments(db.Model):
 
     def __repr__(self):
         return f'Payment: {self.description}'
-
-
-class PaymentsDetails(db.Model):
-    __tablename__ = 'payments_details'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    received = db.Column(db.Numeric(10, 2), nullable=False)
-    change = db.Column(db.Numeric(10, 2), nullable=False)
-    total = db.Column(db.Numeric(10, 2), nullable=False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    payments_id = db.Column(db.Integer, db.ForeignKey(
-        'payments.id', onupdate='restrict', ondelete='cascade'))
-    payment = db.relationship(
-        'Payments', backref=db.backref('payments_details', lazy=True))
-
-    def __init__(self, received, change, total, payment):
-        self.received = received
-        self.change = change
-        self.total = total
-        self.payment = payment
-
-    def __repr__(self):
-        return f'Tipo de Pago {self.payment} Recibido {self.received} Cambio{self.change} Total{self.total}'
