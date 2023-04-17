ea07df774f7d638f7cdbdc66a66f10040c1a45a3
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2023-04-17 01:25:59 -0400

    restructuracion factura y detalles facturas

--- apps/routes/billing.py
+++ apps/routes/billing.py
@@ -1,7 +1,8 @@
-from flask import (render_template, Blueprint, flash, g,
-                   redirect, request, session, url_for,)
 
-import json
+from flask import (render_template, Blueprint, flash, g,
+                   redirect, request, session, url_for, jsonify)
+
+from flask import json
 # Importar el contador
 from itertools import count
 from werkzeug.security import generate_password_hash
@@ -9,18 +10,19 @@ from werkzeug.security import generate_password_hash
 from sqlalchemy import func
 
 from apps.models.billing import Billing, BillingDetail
+
+from apps.models.billing import Factura, DetalleFactura
 from apps.models.user import User
 from apps.models.client import Customer
 from apps.models.company import Company
 from apps.models.employee import Employee
-# from apps.models.payments import Payments
 from apps.models.products import Product
 from apps.models.inventory import Inventory
 from apps.models.orders_services import ServiceOrder
 from apps import db
 from .auth import set_role
 
-billing = Blueprint("billing", __name__, url_prefix="/billing")
+billing = Blueprint("billing", __name__)
 
 
 @billing.route("/list")
@@ -42,45 +44,41 @@ def get_billing(user=None):
 
 
 # Crear un contador que inicie en 100
-order_num_counter = count(start=100)
+# order_num_counter = count(start=100)
 
 
-@billing.route("/create", methods=['GET', 'POST'])
+@billing.route("/billing/create", methods=['GET', 'POST'])
 @set_role
 def create_billing(user=None):
     if request.method == 'POST':
-        # Obtener datos de la cabecera de la factura
-        total = request.form['total']
-        company_id = request.form['company_id']
-        client_id = request.form['client_id']
-        orders_services_id = request.form['orders_services_id']
+        numero = request.form['numero']
+        fecha = request.form['fecha']
+        # Obtener una lista de los valores del campo "detalle"
+        detalles = request.form.getlist('detalle')
+        # Obtener una lista de los valores del campo "cantidad"
+        cantidades = request.form.getlist('cantidad')
+        # Obtener una lista de los valores del campo "precio_unitario"
+        precios_unitarios = request.form.getlist('precio_unitario')
 
-        # Generar el order_num con prefijo "RV-"
-        order_num = "FT-" + str(next(order_num_counter))
-
-        # Fetch los objetos relacionados desde la base de datos
-        company = Company.query.filter_by(id=company_id).first()
-        client = Customer.query.filter_by(id=client_id).first()
-        orders_services = ServiceOrder.query.filter_by(
-            id=orders_services_id).first()
-        # payments = ServiceOrder.query.filter_by(id=payments_id).first()
-
-        # Crear una instancia de la clase Billing
-        billing = Billing(order_num=order_num, total=total, company=company,
-                          client=client, orders_services=orders_services)
-
-        # Guardar la factura en la base de datos
-        db.session.add(billing)
+        factura = Factura(numero=numero, fecha=fecha)
+        db.session.add(factura)
         db.session.commit()
 
-        # Obtener id de la ultima factura
-        max_id = db.session.query(func.max(Billing.id)).scalar()
-        billing_id = max_id
-        billing = Billing.query.filter_by(id=billing_id).first()
+        # Procesar los detalles de la factura
+        for i in range(len(detalles)):
+            factura_id = factura.id
+            descripcion = detalles[i]
+            cantidad = cantidades[i]
+            precio_unitario = precios_unitarios[i]
 
-        # Confirmar los cambios en la base de datos
-        db.session.commit()
-        flash('¡Factura emitida con éxito!')
+            detalle_factura = DetalleFactura(
+                factura_id=factura_id, descripcion=descripcion, cantidad=cantidad, precio_unitario=precio_unitario)
+
+            db.session.add(detalle_factura)
+
+        db.session.commit()  # Commit después de agregar todos los detalles
+
+        flash('Factura creada exitosamente')
         return redirect(url_for('billing.create_billing'))
 
     billings = Billing.query.all()
@@ -88,7 +86,6 @@ def create_billing(user=None):
     customers = Customer.query.all()
     companies = Company.query.all()
     order_service = ServiceOrder.query.all()
-    # payments = Payments.query.all()
     product = db.session.query(Product).join(Inventory).filter(
         Inventory.set_stock > Inventory.min_stock).all()
     if g.role == 'Administrador':
